45a8fde8908d9bcab1b3e9bf617ed903252bee52
unknown <Rogelio.Sales@iam.local>
2018-04-24 17:51:23 -0500

    WorkingOnThePurchaseScript

---
+++ Assets/Scripts/SignPost.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignPost : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject purchasePanel;
+    [SerializeField]
+    private
+
+    // Use this for initialization
+    void Start()
+    {
+        purchasePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Purchase()
+    {
+        purchasePanel.SetActive(true);
+        Time.timeScale = 0;
+
+    }
+    public void Close()
+    {
+        purchasePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
