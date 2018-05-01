7a7bfeb21c91a361471ad08f02fe722840c8528e
unknown <Rogelio.Sales@iam.local>
2018-05-01 17:56:25 -0500

    Dont Play Game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private Text audienceText;
+    [SerializeField]
+    private float audience;
+    [SerializeField]
+    private float minaudience;
+    [SerializeField]
+    private float maxaudience;
+    [SerializeField]
+    private float audienceGained;
+    [SerializeField]
+    private float audienceLoss;
     private MoneyCollector money;
     private MedicalCenter medicine;
     private FoodStorage food;
@@ -12,7 +25,8 @@ public class GameManager : MonoBehaviour
     private int foodValue;
     private int medicineValue;
     private int audienceValue;
-   
+    private bool isHappy;
+    private bool isOver;
     private void Awake()
     {
         money = FindObjectOfType<MoneyCollector>();
@@ -21,26 +35,86 @@ public class GameManager : MonoBehaviour
     }
     private void Start()
     {
+        isHappy = false;
         DataHolding.MoneyGained = 30;
         DataHolding.FoodGained = 10;
         DataHolding.MedicalGained = 5;
+        DataHolding.AudienceCount = 76;
 
+        audienceValue = (int)DataHolding.AudienceCount;
         moneyValue = (int)DataHolding.MoneyGained;
         foodValue = (int)DataHolding.FoodGained;
         medicineValue = (int)DataHolding.MedicalGained;
 
+        audienceText.text = audienceValue.ToString();
         medicine.medicalCount.text = medicineValue.ToString();
         food.foodCount.text = foodValue.ToString();
         money.moneyCount.text = moneyValue.ToString();
-
     }
     private void Update()
     {
         moneyValue = (int)DataHolding.MoneyGained;
         foodValue = (int)DataHolding.FoodGained;
         medicineValue = (int)DataHolding.MedicalGained;
+
         medicine.medicalCount.text = medicineValue.ToString();
         food.foodCount.text = foodValue.ToString();
         money.moneyCount.text = moneyValue.ToString();
+
+        while(audience < 75f)
+        {
+            audience -= audienceLoss * Time.deltaTime;
+            audienceValue = (int)audience;
+            if (audience == minaudience)
+            {
+                isHappy = true;
+                Debug.Log(true);
+                DataHolding.AudienceCount = minaudience;
+                SceneManager.LoadScene(0);
+            }
+        }
+        while (audience > 75f)
+        {
+            audience += audienceLoss * Time.deltaTime;
+            audienceValue = (int)audience;
+            if (audience == maxaudience)
+            {
+                isHappy = true;
+                Debug.Log(true);
+                DataHolding.AudienceCount = maxaudience;
+                SceneManager.LoadScene(0);
+
+            }
+        }
+       
+        //if (audience < 75f)
+        //{
+        //    audience -= audienceLoss * Time.deltaTime;
+        //    audienceValue = (int)audience;
+           
+        //}
+        //else if (audience == minaudience)
+        //{
+        //    isHappy = true;
+        //    Debug.Log(true);
+        //    DataHolding.AudienceCount = minaudience;
+        //    SceneManager.LoadScene(0);
+        //}
+        //else if (audience > 75f)
+        //{
+        //    audience += audienceGained * Time.deltaTime;
+        //    audienceValue = (int)audience;
+        //}
+        //else if (DataHolding.AudienceCount == maxaudience)
+        //{
+        //    DataHolding.AudienceCount = maxaudience;
+        //    SceneManager.LoadScene(0);
+        //}
+        DataHolding.AudienceCount = audience + DataHolding.AudienceCount;
+        audienceValue = (int)DataHolding.AudienceCount;
+        audienceText.text = audienceValue.ToString();
+        audience = 0;
+
     }
+
 }
